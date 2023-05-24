be956bd051764c0b4acceea7b48cb432d53f4eed
david-meltzer <76928255+david-meltzer@users.noreply.github.com>
2023-05-24 16:15:42 -0400

    added comments

--- app.py
+++ app.py
@@ -6,13 +6,11 @@ import streamlit as st
 
 from datasets import load_dataset
 
-#@st.cache_data
 @st.cache
 def read_data(dataset_repo='dhmeltzer/ELI5_embedded'):
     """Read the data from huggingface."""
     return load_dataset(dataset_repo)['train']
 
-#@st.cache_data
 @st.cache
 def load_faiss_index(path_to_faiss="./faiss_index.pickle"):
     """Load and deserialize the Faiss index."""
@@ -21,16 +19,30 @@ def load_faiss_index(path_to_faiss="./faiss_index.pickle"):
     return faiss.deserialize_index(data)
 
 def main():
-    # Load data and models
+    # Load data and FAISS index
     data = read_data()
     faiss_index = load_faiss_index()
 
+    # Checkpoint from Huggingface used to encode Reddit posts.
     model_id="sentence-transformers/all-MiniLM-L6-v2"
     
+    # Use feature-extraction API to get sentence embeddings.
     api_url = f"https://api-inference.huggingface.co/pipeline/feature-extraction/{model_id}"
+    # Token to access Huggingface Inference API.
     headers = {"Authorization": f"Bearer {st.secrets['HF_token']}"}
 
     def query(texts):
+        """
+        Encodes the input using a Huggingface transformer model.
+
+        Input:
+        -------
+        - texts (list): list of strings to encode.
+
+        Output:
+        ------
+        - list of sentence embedding vectors.
+        """
         response = requests.post(api_url, headers=headers, json={"inputs": texts, "options":{"wait_for_model":True}})
         return response.json()
 
@@ -46,36 +58,42 @@ def main():
     We unfortunately cannot verify the veracity of any of the answers posted!""")
 
     
-    # User search
+    # User search with default question.
     user_input = st.text_area("Search box", "What is spacetime made out of?")
 
     # Filters
+    # num_results determines how many results to show the user.
     st.sidebar.markdown("**Filters**")
     num_results = st.sidebar.slider("Number of search results", 1, 50, 5)
 
+    # Vector representation of user input text.
     vector = query([user_input])
+    
     # Fetch results
     if user_input:
-        # Get paper IDs
+        # Get IDs for each search result. 
         _, I = faiss_index.search(np.array(vector).astype("float32"), k=num_results)
         
         # Get individual results
         for id_ in I.flatten().tolist():
             row = data[id_]
             
+            # List of answers for each reddit post.
             answers=row['answers']['text']
+            # URLs listed in each answer.
             answers_URLs = row['answers_urls']['url']
+            
+            # For each answer in answers, replace the placeholders such as '_URL_0_' with the actual URL.
             for k in range(len(answers_URLs)):
                 answers = [answer.replace(f'_URL_{k}_',answers_URLs[k]) for answer in answers]
             
-            
+            # for each search result print the title, what split of the dataset it came from,
+            # the score of the top answer, and the text of the top answer.
             st.write(f"**Title**: {row['title']}")
             st.write(f"**Split**: {row['split']}")
             st.write(f"**Score**: {row['answers']['score'][0]}")
             st.write(f"**Top Answer**: {answers[0]}")
             st.write("-"*20) 
-
-
 
 if __name__ == "__main__":
     main()
